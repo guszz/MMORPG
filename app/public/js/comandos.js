29a5042a2c8786418136c5392315e629eb1e5079
Dilton Neves <diltonfilho@multiplix.net.br>
2019-12-18 17:09:27 -0300

    ajustes em botões cadastro e voltar

--- app/public/js/comandos.js
+++ app/public/js/comandos.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
     window.location.href = '/sair'
   })
 
+  $('#cadastrar').click(function(){
+    window.location.href = '/cadastro'
+  })
+
+  $('#home').click(function(){
+    window.location.href = '/'
+  })
 
   $('#btnAldeoes').click(function(){
     $('#msgErros').hide()
